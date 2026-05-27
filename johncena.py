13e6d5cc1c2d6d1c917c3fcf072e002190fb0bb2
Jay556 <jaheembrown69420@gmail.com>
2026-05-26 21:49:23 -0500

    Update johncena.py

--- johncena.py
+++ johncena.py
@@ -1,16 +1,1 @@
-#!/usr/bin/env python
-
-##########################
-# How to build and execute
-#
-# python johncena.py
-#
-
-import sys
-
-try:
-	if len(sys.argv) == 1:
-	    while True:
-	        print("")
-except KeyboardInterrupt:
-	pass
+a bla bla bla blab 
